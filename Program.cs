c9344a18483dd64f9f4b4050d42e726eea5aef26
Nicholas Balbino <nicholasdev@zohomail.com>
2023-02-24 22:29:35 -0300

    Atualização Adição

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int v1, v2, resultado;
+
+            Console.WriteLine("Digite o primeiro valor para realizar uma soma: ");
+            v1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor: ");
+            v2 = int.Parse(Console.ReadLine());
+            resultado = v1 + v2;
+            Console.WriteLine($"O resultado da sua conta é:" + resultado);
+            Console.ReadKey();
+        }
+    }
+}
+
